6d5b6cad8ce50291678f7c2412f5ff98a7931945
saakshiraut28 <saakshiraut28@gmail.com>
2022-08-27 21:17:55 +0530

    Worked on the hero section

--- src/components/Circle2.jsx
+++ src/components/Circle2.jsx
@@ -7,14 +7,14 @@ import { useState } from 'react';
 
 export default function Circle2() {
     const ref = useRef()
-    useFrame(() => {ref.current.rotation.y += 0.01})
+    //useFrame(() => {ref.current.rotation.z += 0.01})
     const [hover, sethover ] = useState(false)
     return <points ref={ref}
     onPointerOver={(event)=> sethover(true)}
     onPointerOut={(event)=> sethover(false)}
      >
-        <sphereBufferGeometry attach='geometry' args={[2.5]}/>
-        <PointMaterial color="#66F6FF" size={hover?"0.04":"0.001"} />
+        <sphereBufferGeometry attach='geometry' args={[3]}/>
+        <PointMaterial color="#66F6FF" size={hover?"0.03":"0.01"} />
     </points>;
 
 }
--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -5,16 +5,24 @@ import { OrbitControls } from "@react-three/drei";
 
 function Hero() {
   return (
-    <div className="main-container bg-black">
-      <div className="h-screen" >
-        <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 0] }} resize={{ scroll: true, debounce: { scroll: 50, resize: 0 } }}>
+    <div className="main-container bg-gradient-to-r from-[#1c022a] to-[#020726]">
+      <div className="nav bg-white">Navigation</div>
+      {/* _______ 3D CIRCLE _________ */}
+      <div className="relative h-screen">
+        <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 5] }}>
           <OrbitControls enableZoom={false} />
           <ambientLight intensity={0.5} />
           <directionalLight position={[-2, 5, 2]} intensity={0.5} />
           <Circle />
         </Canvas>
       </div>
-      
+      {/** Hero Section______________________________________ */}
+      <div className="hero-section w-full absolute  top-72 md:top-96  text-white">
+        <div className=" grid w-full content-center justify-items-center text-white ">
+          <p className="relative font-mont text-[112px] text-[#f5f5f57d] leading-3">2022</p>
+          <p className="absolute font-oswald text-[42px] text-white leading-6">------ HAPPY HAPPY ------</p>
+        </div>
+      </div>
     </div>
   );
 }
